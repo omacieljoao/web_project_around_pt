489b584d8ede8e76e796ee3310cc3ac998cc04be
João Maciel <jvrabaioli@gmail.com>
2026-04-22 21:55:52 -0300

    open and close edit profile popup

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,45 @@ const initialCards = [
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+
+const editBtn = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const editPopupCloseBtn = editPopup.querySelector(".popup__close");
+const nameInput = editPopup.querySelector(".popup__input_type_name");
+const profileName = document.querySelector(".profile__title");
+const descriptionInput = editPopup.querySelector(
+  ".popup__input_type_description",
+);
+const profileDescription = document.querySelector(".profile__description");
+let formElement = editPopup.querySelector("#edit-profile-form");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal(modal) {
+  openModal(modal);
+  fillProfileForm();
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInputValue = nameInput.value;
+  let descriptionInputValue = descriptionInput.value;
+  profileName.textContent = nameInputValue;
+  profileDescription.textContent = descriptionInputValue;
+  closeModal(editPopup);
+}
+
+editBtn.addEventListener("click", () => handleOpenEditModal(editPopup));
+editPopupCloseBtn.addEventListener("click", () => closeModal(editPopup));
+formElement.addEventListener("submit", handleProfileFormSubmit);
