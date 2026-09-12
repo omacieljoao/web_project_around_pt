90f74c67f72549438561777a9b105217bf3fbdde
João Maciel <jvrabaioli@gmail.com>
2026-09-12 20:29:53 -0300

    started Api.js

---
+++ scripts/Api.js
@@ -0,0 +1,62 @@
+export default class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers,
+    }).then((res) => res.json());
+  }
+
+  getInitialCards() {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: this.headers,
+    }).then((res) => res.json());
+  }
+
+  editProfile(name, about) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    });
+  }
+
+  addNewCard(name, link) {
+    return fetch(`${this.baseUrl}/cards`, {
+      methos: "POST",
+      headers: this.headers,
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    });
+  }
+
+  like() {
+    this.isLiked = !this.isLiked;
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
+  headers: {
+    authorization: "5300bed1-0190-4c64-a95d-32ad375e02ee",
+    "Content-Type": "application/json",
+  },
+});
+
+fetch("https://around-api.pt-br.tripleten-services.com/v1/cards/", {
+  headers: {
+    authorization: "5300bed1-0190-4c64-a95d-32ad375e02ee",
+  },
+})
+  .then((res) => res.json())
+  .then((result) => {
+    console.log(result);
+  });
